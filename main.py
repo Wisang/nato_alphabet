0288405e6bbdc5a6e347e46b2c5a2d9ad213e91b
wisang <wseom7@gmail.com>
2022-02-11 12:47:21 +0900

    error handling added

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 import pandas
 
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
-print(df)
-
 nato_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
-print(nato_dict)
 
-name = input("What is your name? ").upper()
+def generate_nano_alphabet():
+    name = input("What is your name? ").upper()
+    try:
+        result = [nato_dict[char] for char in name]
+    except KeyError:
+        print("wrong input")
+        generate_nano_alphabet()
+    else:
+        print(result)
 
 
-result = [nato_dict[char] for char in name]
-
-print(result)
+generate_nano_alphabet()
